734b8c02c6d78c4daef38bdd80331010a3257dad
unknown <fnarayev@gmail.com>
2024-06-03 22:32:31 +0300

    init: create my-app project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header () {
+    return (
+        <header className='header'> 
+            <img className='logo-img' src='https://i.pinimg.com/originals/14/6e/0f/146e0f3a0694840f9349e5edf0e7b1f1.png'/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar () {
+    return (
+        <nav className='nav'>
+            <p>Profile</p>
+            <p>Messages</p>
+            <p>News</p>
+            <p>Music</p>
+            <p>Settings</p>
+        </nav>
+    )
+}
+
+export default Navbar
